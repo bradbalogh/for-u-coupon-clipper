b5edfeed5fcc7acad0ea0cceb755bd080843a597
bbalogh <bbalogh@vailsys.com>
2025-03-04 10:44:41 -0600

    add loading indicator

--- scripts/popups.js
+++ scripts/popups.js
@@ -1,21 +1,36 @@
 document.getElementById("click-button").addEventListener("click", () => {
+
+  const button = document.getElementById("click-button");
+  const spinner = document.getElementById("loading-spinner");
+
+  button.disabled = true;
+  spinner.style.display = "inline-block";
+
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
       function: clipCoupons
+    }).then(() => {
+      button.disabled = false;
+      spinner.style.display = "none";
+    }).catch(error => {
+      console.error("Error executing script:", error);
+      button.disabled = false;
+      spinner.style.display = "none";
     });
   });
+
 });
 
 function clipCoupons() {
 
-  function clickLoadMoreButtons() {
+  function clickLoadMoreButtons(callback) {
     const button = document.querySelector('button[class^="btn load-more"]');
     if (button) {
       button.click();
-      setTimeout(clickLoadMoreButtons, 10);
+      setTimeout(() => clickLoadMoreButtons(callback), 10);
     } else {
-      clipAllCoupons();
+      callback();
     }
   }
 
@@ -24,5 +39,11 @@ function clipCoupons() {
     clipButtons.forEach(button => button.click());
   }
 
-  clickLoadMoreButtons();
+  return new Promise(resolve => {
+    clickLoadMoreButtons(() => {
+      clipAllCoupons();
+      resolve();
+    });
+  });
+
 }
